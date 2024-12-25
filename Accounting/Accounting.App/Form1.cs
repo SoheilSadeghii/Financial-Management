0cee207745f62759f6eadbb822d4a179fa4d7300
SoheilSadeghii <sohel.sadeghi.2003@gmail.com>
2024-12-25 20:18:05 +0330

    adding files

---
+++ Accounting/Accounting.App/Form1.cs
@@ -0,0 +1,102 @@
+﻿using Accounting.Business;
+using Accounting.Utility.Conventor;
+using Accounting.ViewModels.Accounting;
+using Stimulsoft.Report.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accounting.App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            frmCustomers frmCustomer = new frmCustomers();
+            frmCustomer.ShowDialog();
+        }
+
+        private void btnNewAccounting_Click(object sender, EventArgs e)
+        {
+            frmNewAccounting frmNewAccounting = new frmNewAccounting();
+            //frmNewAccounting.ShowDialog();
+
+            if (frmNewAccounting.ShowDialog() == DialogResult.OK)
+            {
+                BindGrid();
+            }
+        }
+
+        private void btnReportPay_Click(object sender, EventArgs e)
+        {
+            frmReport frmReport = new frmReport();
+            frmReport.TypeID = 2;
+            frmReport.ShowDialog();
+        }
+
+        private void btnReportRecive_Click(object sender, EventArgs e)
+        {
+            frmReport frmReport = new frmReport();
+            frmReport.TypeID = 1;
+            frmReport.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmLogin frmLogin = new frmLogin();
+            if (frmLogin.ShowDialog() == DialogResult.OK)
+            {
+                this.Show();
+                lblDate.Text = DateTime.Now.ToShamsi();
+                lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+                Report();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        void Report()
+        {
+            BindGrid();
+        }
+
+        private void BindGrid()
+        {
+            ReportViewModel report = Account.ReportFormMain();
+            lblPay.Text = report.Pay.ToString("#,0");
+            lblReceive.Text = report.Receive.ToString("#,0");
+            lblAccountBalance.Text = report.AccountBalance.ToString("#,0");
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void btnEditLogin_Click(object sender, EventArgs e)
+        {
+            frmLogin frmLogin = new frmLogin();
+            frmLogin.IsEdit = true;
+            frmLogin.ShowDialog();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+    }
+}
---
+++ Accounting/Accounting.Utility/Conventor/Amount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accounting.Utility.Conventor
+{
+    public static class Amount
+    {
+        public static void ToRial(this NumericUpDown numericUpDown)
+        {
+            // Temporarily store the current value
+            decimal value = numericUpDown.Value;
+
+            // Set the thousands separator format
+            numericUpDown.ThousandsSeparator = true;
+
+            // Update the value to trigger the formatting
+            numericUpDown.Value = value;
+        }
+    }
+}
